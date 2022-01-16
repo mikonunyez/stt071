74c704b6e6bafb1ec7acdf3d80e02cf640da4c38
Miko Nunez <nunyezmiko@gmail.com>
2022-01-17 06:19:07 +0800

    finished items no. 1, 4, 5 on exercise 3

--- r-programming/Nunez-STT071.1 Exercise 3.R
+++ r-programming/Nunez-STT071.1 Exercise 3.R
@@ -1,1 +1,25 @@
 ### Question no.1 ###
+# poisson
+
+# a
+ppois(2, 3)
+
+#b
+ppois(1, 3, lower=FALSE)
+
+
+### Question no.2 ###
+
+
+### Question no.3 ###
+
+
+### Question no.4 ###
+phyper(2, 9, 6, 4)
+
+
+### Question no.5 ###
+dnbinom(5, 3, 0.2) #temp
+
+
+### Question no.6 ###
