888a31f7a3cba0d6d4a157e7cb8eb909897ea666
Miko Nunez <nunyezmiko@gmail.com>
2022-01-17 14:52:19 +0800

    added temporary answers for items 2,3 and 6 for exercise 3

--- r-programming/Nunez-STT071.1 Exercise 3.R
+++ r-programming/Nunez-STT071.1 Exercise 3.R
@@ -9,16 +9,22 @@ ppois(1, 3, lower=FALSE)
 
 
 ### Question no.2 ###
-
+# binomial..temp
+dbinom(5, 10, 0.3)
 
 ### Question no.3 ###
-
+# negative binomial..temp
+dnbinom(2, 5, 0.5)
 
 ### Question no.4 ###
+# hypergeometric
 phyper(2, 9, 6, 4)
 
 
 ### Question no.5 ###
+# binomial
 dbinom(3, 5, 0.2)
 
 ### Question no.6 ###
+# binomial..temp
+1-pbinom(23,25, 0.97)
